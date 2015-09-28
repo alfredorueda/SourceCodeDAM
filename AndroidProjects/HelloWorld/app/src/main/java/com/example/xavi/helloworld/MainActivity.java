2c0b1be40ffe1fe40c7875f7e0c83b50631d24ba
Xavi Moll <xmollv@gmail.com>
2015-09-28 18:19:23 +0200

    Android.

--- AndroidProjects/HelloWorld/app/src/main/java/com/example/xavi/helloworld/MainActivity.java
+++ AndroidProjects/HelloWorld/app/src/main/java/com/example/xavi/helloworld/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.xavi.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "Hola", Toast.LENGTH_LONG).show();
+            Log.i("Xavi","Has hecho settings!");
             return true;
         }
 
