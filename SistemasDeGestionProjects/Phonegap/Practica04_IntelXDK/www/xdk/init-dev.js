a7d5d0c18ade9712e0a3622233fd8b34dd6e272d
Xavi Moll <xmollv@gmail.com>
2015-11-20 21:34:28 +0100

    Phonegap.

--- SistemasDeGestionProjects/Phonegap/Practica04_IntelXDK/www/xdk/init-dev.js
+++ SistemasDeGestionProjects/Phonegap/Practica04_IntelXDK/www/xdk/init-dev.js
@@ -239,6 +239,7 @@ dev.initDeviceReady = function() {
     dev.consoleLog(fName, "navigator.userAgent:", navigator.userAgent) ;
 
     dev.consoleLog(fName, "exit") ;
+    $("#botonClick").click(hola)
 } ;
 
 
