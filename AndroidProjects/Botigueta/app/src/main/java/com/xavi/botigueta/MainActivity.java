711ecca1917a306dec7c022c76d3d7e6798bd8a0
Xavi Moll <xmollv@gmail.com>
2015-12-22 19:38:39 +0100

    Android project list view.

--- AndroidProjects/Botigueta/app/src/main/java/com/xavi/botigueta/MainActivity.java
+++ AndroidProjects/Botigueta/app/src/main/java/com/xavi/botigueta/MainActivity.java
@@ -1,13 +1,19 @@
 package com.xavi.botigueta;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,22 +25,143 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<Products> products = new ArrayList();
+    List<Products> productsShoppingCart = new ArrayList();
+    Spinner spinner;
+    SpinnerAdapter spinnerAdapter;
+    Button addButton;
+
+    ListView listShoppingCart;
+    ShoppingCartAdapter shoppingCartAdapter;
+    String[] values;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] rawData = getResources().getStringArray(R.array.food_array);
-        final List<String> productes = new ArrayList<>();
-
+        /*String[] rawData = getResources().getStringArray(R.array.food_array);
         for (int i = 0; i < rawData.length; i++){
-            productes.add(rawData[i].split(":")[1]);
-        }
+            values = rawData[i].split(":");
+            //System.out.println(values[4].split("\\.")[0].toString());
+            products.add(new Products(Integer.parseInt(values[0]), values[1], Double.parseDouble(values[3]), Integer.parseInt("R.drawable."+values[4].split("\\.")[0]), 0));
+        }*/
+
+        products.add(new Products(0, "Poma", 1.56, R.drawable.poma, 0));
+        products.add(new Products(1, "Plàtan", 0.15, R.drawable.platan, 0));
+        products.add(new Products(2, "Pastís", 12.99, R.drawable.pastis, 0));
 
         //Populate spinner with data
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, productes);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinnerAdapter = new SpinnerAdapter(this, products);
+        spinner.setAdapter(spinnerAdapter);
+
+        listShoppingCart = (ListView) findViewById(R.id.listView);
+        shoppingCartAdapter = new ShoppingCartAdapter(this, productsShoppingCart);
+
+
+        addButton = (Button) findViewById(R.id.button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = spinner.getSelectedItemPosition();
+                boolean repeated = false;
+                int i = 1;
+
+                if (productsShoppingCart.isEmpty()){
+                    productsShoppingCart.add(new Products(products.get(id).getId(), products.get(id).getName(), products.get(id).getPrice(), products.get(id).getImage(), 1));
+                } else {
+                    for(Products product: productsShoppingCart){
+                        if (product.getId() == id){
+                            repeated = true;
+                            productsShoppingCart.get(i-1).setQuantity(productsShoppingCart.get(i-1).getQuantity()+1);
+                            break;
+                        }
+                        if (i == productsShoppingCart.size() && repeated == false){
+                            productsShoppingCart.add(new Products(products.get(id).getId(), products.get(id).getName(), products.get(id).getPrice(), products.get(id).getImage(), 1));
+                        }
+                        i++;
+                    }
+                }
+                listShoppingCart.setAdapter(shoppingCartAdapter);
+            }
+        });
+    }
+
+
+    private class SpinnerAdapter extends BaseAdapter{
+
+        private Context context;
+        List<Products> productsSpinner;
+
+        public SpinnerAdapter(Context context, List<Products> productsSpinner) {
+            this.context = context;
+            this.productsSpinner = productsSpinner;
+        }
+
+        @Override
+        public int getCount() {
+            return productsSpinner.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return productsSpinner.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = ((LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.spinner_item, null);
+            }
+            ((TextView) convertView.findViewById(R.id.productNameSpinner)).setText(productsSpinner.get(position).getName());
+            ((TextView) convertView.findViewById(R.id.productPriceSpinner)).setText(String.valueOf(productsSpinner.get(position).getPrice()));
+            return convertView;
+        }
+    }
+
+    private class ShoppingCartAdapter extends BaseAdapter{
+        private Context context;
+        List<Products> productsShoppingCart;
+
+        public ShoppingCartAdapter(Context context, List<Products> productsShoppingCart) {
+            this.context = context;
+            this.productsShoppingCart = productsShoppingCart;
+        }
+
+
+        @Override
+        public int getCount() {
+            return productsShoppingCart.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return productsShoppingCart.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = ((LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.shopping_cart_item, null);
+            }
+            ((TextView) convertView.findViewById(R.id.shoppingCartName)).setText(productsShoppingCart.get(position).getName());
+            ((TextView) convertView.findViewById(R.id.quantity)).setText(String.valueOf(productsShoppingCart.get(position).getQuantity() + " x "));
+            ((TextView) convertView.findViewById(R.id.price)).setText(String.valueOf(productsShoppingCart.get(position).getPrice()));
+            ((ImageView) convertView.findViewById(R.id.imageView)).setImageResource(productsShoppingCart.get(position).getImage());
+            return convertView;
+        }
     }
 }
---
+++ AndroidProjects/Botigueta/app/src/main/java/com/xavi/botigueta/Products.java
@@ -0,0 +1,60 @@
+package com.xavi.botigueta;
+
+/**
+ * Created by Xavi on 22/12/15.
+ */
+public class Products {
+    int id;
+    String name;
+    Double price;
+    int image;
+    int quantity;
+
+    public Products(int id, String name, Double price, int image, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.image = image;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
